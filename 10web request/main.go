b1e7081be51fecf4cf6f33a9f603eb5666efbcc0
manikantkg <manikantacoder007@gmail.com>
2023-11-22 07:37:48 +0530

    web requests

---
+++ 10web request/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "https://www.mpstdc.com/" //"https://lco.dev" //"https://google.co.in"
+
+func main() {
+	fmt.Println("WeB request")
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Resp is of type: %T\n", resp)
+	// fmt.Println("respoinse is >>", resp)
+
+	defer resp.Body.Close() //caller's responsibility to close the content
+
+	databytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(databytes)
+	fmt.Println(content)
+}
