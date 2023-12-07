f436f413b678f48dbee83bf41277db839bfce5bf
manikantkg <manikantacoder007@gmail.com>
2023-12-07 10:01:47 +0530

    server is not running

--- 12server/main.go
+++ 12server/main.go
@@ -4,32 +4,53 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
+	"net/url"
 )
 
 func main() {
 	fmt.Println("Welocme to web API requests")
 
-	PerformGetRequest()
+	// PerformGetRequest()
+	// PerforPostJsonRequest()
+	PerformPostFormGetRequest()
 }
 
 func PerformGetRequest() {
-	const url = "http://localhost:8000/get"
 
-	resp, err := http.Get(url)
+}
+
+func PerforPostJsonRequest() {
+
+}
+
+func PerformPostFormGetRequest() {
+	const myurl = "http://localhost:8000/get"
+
+	//formdata
+	data := url.Values{}
+	data.Add("firstname", "mani")
+	data.Add("lastname", "kanta")
+	data.Add("email", "mani@123.com")
+
+	resp, err := http.PostForm(myurl, data)
 	if err != nil {
 		panic(err)
 	}
+
+	// resp, err := http.Get(url)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	defer resp.Body.Close()
 
 	fmt.Println("Status code : ", resp.StatusCode)
 	fmt.Println("Content length: ", resp.ContentLength)
 
-	var respString strings.Builder
+	// var respString strings.Builder
 
 	content, _ := ioutil.ReadAll(resp.Body)
-	byteCount, _ := respString.Write(content)
-	// fmt.Println(string(content))
-	fmt.Println("ByteCount", byteCount)
+	// byteCount, _ := respString.Write(content)
+	fmt.Println(string(content))
+	// fmt.Println("ByteCount", byteCount)
 }
