0cd1bae749bb6eca795456c7241ddb9847269f3c
manikantkg <manikantacoder007@gmail.com>
2024-01-23 11:22:24 +0530

    pyramid programme added

--- programe/main.go
+++ programe/main.go
@@ -3,15 +3,41 @@ package main
 import "fmt"
 
 func main() {
-	Fact(4)
+	//Fact(4)
+	//numbers()
+	pyramid()
 
 }
 
 func Fact(n int) {
 	var a int = 1
 	for i := 1; i <= n; i++ {
-		a *= i
+		// a *= i
+		a = a * i
 		fmt.Println(a)
+	}
+
+}
+
+func numbers() { //need to check the output
+	for i := 1; i <= 10; i++ {
+		i *= i
+		fmt.Println(i)
+
+	}
+}
+
+func pyramid() {
+	rows := 5
+
+	for i := 1; i < rows; i++ {
+		for j := 1; j < rows-i; j++ {
+			fmt.Print(" ")
+		}
+		for k := 0; k != (2*i - 1); k++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
 	}
 
 }
