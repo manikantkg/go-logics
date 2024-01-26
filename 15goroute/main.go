f3250d540985fd765ba9acd12571a09fbba65be9
manikantkg <manikantacoder007@gmail.com>
2024-01-26 21:56:27 +0530

    channels passed

--- 15goroute/main.go
+++ 15goroute/main.go
@@ -5,22 +5,34 @@ import (
 	"time"
 )
 
-func Add(x, y int) {
-	C := x + y
-	fmt.Println("c is :", C)
-}
-
-func Sum(a chan int) {
-	var b, c int
-	a <- a
-	c = a + b
+/*
+	 func Add(x, y int) {
+		C := x + y
+		fmt.Println("c is :", C)
+	}
+*/
+func sum(p chan int) {
+	var b int = 23
+	//p = make(chan int)
+	a := <-p
+	c := a + b
 	fmt.Println("c is:", c)
 }
 
 func main() {
-	fmt.Println("Goroutineds Stats from here!!!")
+	/* fmt.Println("Goroutineds Stats from here!!!")
 	go Add(3, 5)                //simple go routine
 	time.Sleep(2 * time.Second) //holds the main function execution by using time package
-	fmt.Println("Goroutineds Ends from here!!!")
+	fmt.Println("Goroutineds Ends from here!!!") */
+
+	fmt.Println(">>>>>>>>>>>>>>")
+
+	d := make(chan int)
+	//fmt.Println(d)
+	d <- 3
+	panic("Error occured")
+	fmt.Println(<-d)
+	go sum(d)
+	time.Sleep(2 * time.Second)
 
 }
