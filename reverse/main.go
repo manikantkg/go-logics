f99d165a1042bc2fc4ded6206dec84ccd73f85a9
manikantkg <manikantacoder007@gmail.com>
2024-07-05 14:33:36 +0530

    reverse second method added

--- reverse/main.go
+++ reverse/main.go
@@ -6,6 +6,7 @@ func main() {
 	str := "Manikanta"
 	fmt.Println(str)
 	fmt.Println(Reverse(str))
+	fmt.Println(Rev(str))
 
 }
 
@@ -14,4 +15,12 @@ func Reverse(str string) (result string) {
 		result = string(v) + result
 	}
 	return
+}
+
+func Rev(s string) string {
+	rns := []rune(s)
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+	return string(rns)
 }
