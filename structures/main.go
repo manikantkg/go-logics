32a3713900c390f4bb0b2cc2ebfc698afa063755
manikantkg <manikantacoder007@gmail.com>
2024-07-12 19:39:54 +0530

    fetching specific structs data

--- structures/main.go
+++ structures/main.go
@@ -39,7 +39,19 @@ func main() {
 	for _, person := range p {
 		fmt.Println(person)
 	}
+	//printing indidvidual elements
+	fmt.Println("First Person Details:")
+	fmt.Println("Name:", p[0].Name)
+	fmt.Println("Location:", p[0].Loc)
+	fmt.Println("State:", p[0].State)
+	fmt.Println() // Print a blank line for separation
 
+	// Print the last person's details
+	fmt.Println("Last Person Details:")
+	fmt.Println("Name:", p[len(p)-1].Name)
+	fmt.Println("Location:", p[len(p)-1].Loc)
+	fmt.Println("State:", p[len(p)-1].State)
+	//
 	mani := User{"Manikanta", 33, "manikanta@123.com", true}
 	//fmt.Println(mani)
 	fmt.Printf("Mani detials are : %+v\n", mani)
