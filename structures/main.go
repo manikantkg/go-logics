4d6077fad8ea4b6cca64247b06e9230d547c7ec1
manikantkg <manikantacoder007@gmail.com>
2024-07-12 19:37:26 +0530

    structs modified

--- structures/main.go
+++ structures/main.go
@@ -10,10 +10,38 @@ type User struct {
 }
 
 //No inheritence && treated as class or parent
+type Person struct {
+	Name  string
+	Loc   string
+	State string
+}
+
 func main() {
+
+	// var p []Person
+	p := []Person{
+		{
+			Name:  "ABC",
+			Loc:   "HYD",
+			State: "TS",
+		},
+		{Name: "BC",
+			Loc:   "HYD",
+			State: "TS",
+		},
+		{
+			Name:  "ABC",
+			Loc:   "HYD",
+			State: "TS",
+		},
+	}
+
+	for _, person := range p {
+		fmt.Println(person)
+	}
+
 	mani := User{"Manikanta", 33, "manikanta@123.com", true}
 	//fmt.Println(mani)
 	fmt.Printf("Mani detials are : %+v\n", mani)
 	fmt.Printf("Mani name is  %v and Email is: %v", mani.Name, mani.Email)
-
 }
