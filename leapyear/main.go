50bc864e73d7e5fa2aa022ec2b2ae3ae612fb4f5
manikantkg <manikantacoder007@gmail.com>
2024-07-18 18:51:54 +0530

    count upper case letters added

--- leapyear/main.go
+++ leapyear/main.go
@@ -11,4 +11,19 @@ func main() {
 	} else {
 		fmt.Println("Not Leap year")
 	}
+	//count upper case function
+	CountUpperCase()
+}
+
+//count uppercase letters
+func CountUpperCase() {
+
+	a := "My Name Is ManiMantA" //M-N-I-M-M-A
+	var count int = 0
+	for _, v := range a {
+		if v >= 65 && v <= 90 {
+			count++
+		}
+	}
+	fmt.Println("Count is : ", count)
 }
--- makenew/main.go
+++ makenew/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 
 	a := make([]int, 3)
-	fmt.Println(a, len(a), cap(a), &a)
+	fmt.Println(a, "len is :", len(a), "cap is : ", cap(a), &a)
 
 	b := [5]int{1, 2, 3, 4, 5}
 	fmt.Println(&b[1])
